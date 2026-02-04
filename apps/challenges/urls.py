e285c176e19802ac7f133f3bf2046d9318fd825f
0Mart00 <nyiri.martin.101@gmail.com>
2026-02-04 09:42:27 +0100

    Urls updated

--- apps/challenges/urls.py
+++ apps/challenges/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', DashboardView.as_view(), name='dashboard'),
     
     # Az API végpont a létrehozáshoz (JSON)
+    path('create/', GadgetCreateView.as_view(), name='gadget-create-form'),
     path('api/create/', GadgetAPIView.as_view(), name='gadget-create'),
     path('gadget/<slug:slug>/', GadgetDetailView.as_view(), name='detail'),
     path('gadget/<int:pk>/vote/', GadgetVoteView.as_view(), name='gadget-vote'),
